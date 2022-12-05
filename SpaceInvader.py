fb33b84077a1a4c7a25127371a8ce6d35ecae945
Marchandon Lise <lise.marchandon@cpe.fr>
2022-12-05 14:27:56 +0100

    f

--- SpaceInvader.py
+++ SpaceInvader.py
@@ -1,1 +1,17 @@
-from tkinter import *
+from tkinter import Tk, Button, Label, StringVar, Frame, PhotoImage
+
+MaFenetre = Tk()
+MaFenetre.title('Space Invader')
+
+MaFenetre.geometry('1000x800+400+200')
+
+photo = PhotoImage(file="ciel-étoile.jpg")
+
+FrameMenu = Frame (MaFenetre)
+FrameMenu.pack(side= 'top', width = 5, height = 5 )
+
+
+
+
+
+MaFenetre.mainloop()
